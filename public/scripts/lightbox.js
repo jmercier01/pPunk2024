d2aa14295b8a04e965ef76c8119f53e3f97e9fd7
Jerome Mercier <jerom.mercier@gmail.com>
2024-10-24 11:36:23 +0200

    Modifs Alt images

--- public/scripts/lightbox.js
+++ public/scripts/lightbox.js
@@ -1,13 +1,17 @@
-console.log('lightbox');
+// console.log('lightbox');
 const thumbnails = [...document.querySelectorAll(".thumbnail")];
   const lightBox = document.querySelector(".lightbox");
   const lightBoxImg = document.querySelector("#lightbox-img");
   const lightBoxClose = document.querySelector("#lightbox-close-btn");
+  const lightboxAlt = document.querySelector("#lightbox-alt");
 
   thumbnails.map((thumbnail) => {
     thumbnail.addEventListener("click", function (e) {
       const imageUrl = thumbnail.dataset.src;
+      const imageAlt = thumbnail.alt;
       lightBoxImg.src = imageUrl;
+      lightBoxImg.alt = imageAlt;
+      lightboxAlt.textContent = imageAlt;
       lightBox.classList.remove("none");
       lightBox.classList.add("active");
     });
@@ -15,11 +19,14 @@ const thumbnails = [...document.querySelectorAll(".thumbnail")];
   lightBoxClose.addEventListener("click", function (e) {
     lightBox.classList.remove("active");
     lightBox.classList.add("none");
+    lightboxAlt.textContent = "";
+    
   });
   lightBox.addEventListener("click", function (e) {
     if (e.target === lightBox) {
       // Vérifie que le clic est en dehors de l'image
       lightBox.classList.remove("active"); // Cache la lightbox
       lightBox.classList.add("none");
+      lightboxAlt.textContent = "";
     }
   });
